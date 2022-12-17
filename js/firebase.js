04434669a5dca9bd800877e59aa6ceebcaf90409
Vinícius Lima <84545534+viniciusp-lima@users.noreply.github.com>
2022-12-17 18:34:30 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxvhYa-btejU6aIRFP3WUNOqOB1Xt01_w",
+  authDomain: "todolistvl.firebaseapp.com",
+  databaseURL: "https://todolistvl-default-rtdb.firebaseio.com/",
+  projectId: "todolistvl",
+  storageBucket: "todolistvl.appspot.com",
+  messagingSenderId: "1099471367580",
+  appId: "1:1099471367580:web:47476f1f90148180a96c59"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
