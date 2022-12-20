fb8f5a5fc830392dd68b5338cec002a55830918f
Vinícius Lima <84545534+viniciusp-lima@users.noreply.github.com>
2022-12-20 09:33:34 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -13,17 +13,15 @@ function switchForm() {
     }
 }
 
-function mensage(msg='', classe='', time=5000) {
+function mensage(msg='', time=5000) {
     const mensage = document.querySelector('.mensage');
 
     mensage.innerHTML = msg;
     mensage.classList.add('active');
-    mensage.classList.add(classe);
 
     if(mensage.classList.contains('active')) {
         setInterval(() => {
             mensage.classList.remove('active');
-            mensage.classList.remove(classe)
         }, time);
     }
 }
